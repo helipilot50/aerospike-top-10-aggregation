23e40878512ef1a9c78d72bf225385471d18d57d
Peter Milne <helipilot50@gmail.com>
2015-01-22 12:07:56 -0600

    Initial project generation

---
+++ src/main/java/com/aerospike/examples/TopTen.java
@@ -0,0 +1,104 @@
+package com.aerospike.examples;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.log4j.Logger;
+
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.policy.Policy;
+import com.aerospike.client.policy.WritePolicy;
+
+
+/**
+@author Peter Milne
+*/
+public class TopTen {
+	private AerospikeClient client;
+	private String seedHost;
+	private int port;
+	private String namespace;
+	private String set;
+	private WritePolicy writePolicy;
+	private Policy policy;
+
+	private static Logger log = Logger.getLogger(TopTen.class);
+	public TopTen(String host, int port, String namespace, String set) throws AerospikeException {
+		this.client = new AerospikeClient(host, port);
+		this.seedHost = host;
+		this.port = port;
+		this.namespace = namespace;
+		this.set = set;
+		this.writePolicy = new WritePolicy();
+		this.policy = new Policy();
+	}
+	public TopTen(AerospikeClient client, String namespace, String set) throws AerospikeException {
+		this.client = client;
+		this.namespace = namespace;
+		this.set = set;
+		this.writePolicy = new WritePolicy();
+		this.policy = new Policy();
+	}
+	public static void main(String[] args) throws AerospikeException {
+		try {
+			Options options = new Options();
+			options.addOption("h", "host", true, "Server hostname (default: 127.0.0.1)");
+			options.addOption("p", "port", true, "Server port (default: 3000)");
+			options.addOption("n", "namespace", true, "Namespace (default: test)");
+			options.addOption("s", "set", true, "Set (default: demo)");
+			options.addOption("u", "usage", false, "Print usage.");
+
+			CommandLineParser parser = new PosixParser();
+			CommandLine cl = parser.parse(options, args, false);
+
+
+			String host = cl.getOptionValue("h", "127.0.0.1");
+			String portString = cl.getOptionValue("p", "3000");
+			int port = Integer.parseInt(portString);
+			String namespace = cl.getOptionValue("n", "test");
+			String set = cl.getOptionValue("s", "demo");
+			log.debug("Host: " + host);
+			log.debug("Port: " + port);
+			log.debug("Namespace: " + namespace);
+			log.debug("Set: " + set);
+
+			@SuppressWarnings("unchecked")
+			List<String> cmds = cl.getArgList();
+			if (cmds.size() == 0 && cl.hasOption("u")) {
+				logUsage(options);
+				return;
+			}
+
+			TopTen as = new TopTen(host, port, namespace, set);
+
+			as.work();
+
+		} catch (Exception e) {
+			log.error("Critical error", e);
+		}
+	}
+	/**
+	 * Write usage to console.
+	 */
+	private static void logUsage(Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		String syntax = TopTen.class.getName() + " [<options>]";
+		formatter.printHelp(pw, 100, syntax, "options:", options, 0, 2, null);
+		log.info(sw.toString());
+	}
+
+	public void work() throws Exception {
+		// TODO add your code here
+	}
+
+}
